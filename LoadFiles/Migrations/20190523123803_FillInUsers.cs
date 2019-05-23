354453e1b0e082f48a9e7ed3c5805e5a65d99279
AlexL70 <alexander.levinson.70@gmail.com>
2019-05-23 15:47:04 +0300

    Fill in Users table

---
+++ LoadFiles/Migrations/20190523123803_FillInUsers.cs
@@ -0,0 +1,22 @@
+﻿using LoadFiles.Core.Models;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LoadFiles.Migrations
+{
+    public partial class FillInUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO [dbo].[Users] ([{nameof(User.Name)}]) VALUES ('User1')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[Users] ([{nameof(User.Name)}]) VALUES ('User2')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[Users] ([{nameof(User.Name)}]) VALUES ('User3')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[Users] ([{nameof(User.Name)}]) VALUES ('User4')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[Users] ([{nameof(User.Name)}]) VALUES ('User5')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM [dbo].[Users]");
+        }
+    }
+}
